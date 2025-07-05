73435cd477724ddf1474779cc431e239925893ea
osuigbagoodness2 <112334960+osuigbagoodness2@users.noreply.github.com>
2025-07-05 23:10:02 +0100

    first wdd131

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Set the current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Set the last modified date
+document.getElementById("lastModified").textContent = Last Modified: ${document.lastModified};
